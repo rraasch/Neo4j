b05569521476ea462f19ec802548b29d41ba1eb8
Ryan Raasch <ryanraasch94@gmail.com>
2022-06-09 11:48:17 -0400

    Add files via upload

---
+++ neo4j-python-driver.py
@@ -0,0 +1,19 @@
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "neo4j://44.202.204.14:7687",
+  auth=basic_auth("neo4j", "rifling-ambiguity-elbow"))
+
+cypher_query = '''
+MATCH (a:Airport{iata:$iata})-[r:HAS_ROUTE]->(other)
+  RETURN other.iata as destination
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+                      iata="DEN").data())
+  for record in results:
+    print(record['destination'])
+
+driver.close()
